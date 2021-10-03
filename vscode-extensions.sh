1b50638f4043e88ba61a1f52c20ee1096b932b0e
Thien Do <hi.thiendo@icloud.com>
2021-10-03 19:26:33 +0700

    Add vscode extensions

---
+++ vscode-extensions.sh
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/questions/35773299/how-can-you-export-the-visual-studio-code-extension-list
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.github-vscode-theme
+code --install-extension mvllow.rose-pine
+code --install-extension vscodevim.vim
